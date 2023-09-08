5ca5bb5d3fc5614ec9656bbc0dcc05c568e15249
Alejandro Palacio <alejandrooss94@gmail.com>
2023-09-07 20:30:12 -0500

    avances API

--- src/main/java/com/alejopp/springbootdocker/controllers/UserController.java
+++ src/main/java/com/alejopp/springbootdocker/controllers/UserController.java
@@ -1,10 +1,37 @@
 package com.alejopp.springbootdocker.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alejopp.springbootdocker.entities.User;
+import com.alejopp.springbootdocker.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
 }
